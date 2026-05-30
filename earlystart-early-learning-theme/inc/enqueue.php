dcefae3e7e83c622a60e5f025663bcfdf2631904
charancre-del <charancre-del@users.noreply.github.com>
2026-05-30 18:18:27 -0400

    Dequeue unused WordPress global styles

--- earlystart-early-learning-theme/inc/enqueue.php
+++ earlystart-early-learning-theme/inc/enqueue.php
@@ -370,6 +370,8 @@ function earlystart_dequeue_block_styles()
         if (!is_admin()) {
                 wp_dequeue_style('wp-block-library');
                 wp_dequeue_style('wp-block-library-theme');
+                wp_dequeue_style('global-styles');
+                wp_dequeue_style('classic-theme-styles');
                 wp_dequeue_style('wc-blocks-style'); // WooCommerce
         }
 }
