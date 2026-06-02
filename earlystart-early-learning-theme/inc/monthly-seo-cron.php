375f433d67099e29d93c6b9258e606b65ac4f293
charancre-del <charancre-del@users.noreply.github.com>
2026-06-02 19:46:01 -0400

    Fix monthly sitemap ping target

--- earlystart-early-learning-theme/inc/monthly-seo-cron.php
+++ earlystart-early-learning-theme/inc/monthly-seo-cron.php
@@ -30,10 +30,21 @@ add_filter('cron_schedules', 'earlystart_add_monthly_cron_interval');
 function earlystart_activate_monthly_seo_cron()
 {
 	if (!wp_next_scheduled('earlystart_monthly_seo_event')) {
-		wp_schedule_event(time(), 'monthly', 'earlystart_monthly_seo_event');
+		wp_schedule_event(time() + HOUR_IN_SECONDS, 'monthly', 'earlystart_monthly_seo_event');
 	}
 }
 add_action('after_switch_theme', 'earlystart_activate_monthly_seo_cron');
+
+/**
+ * Ensure the monthly SEO event exists after deploys and restores.
+ */
+function earlystart_ensure_monthly_seo_cron()
+{
+	if (!wp_next_scheduled('earlystart_monthly_seo_event')) {
+		earlystart_activate_monthly_seo_cron();
+	}
+}
+add_action('init', 'earlystart_ensure_monthly_seo_cron');
 
 /**
  * Unschedule on Theme Deactivation
@@ -48,6 +59,34 @@ function earlystart_deactivate_monthly_seo_cron()
 add_action('switch_theme', 'earlystart_deactivate_monthly_seo_cron');
 
 /**
+ * Get the canonical sitemap URL used by recurring SEO pings.
+ */
+function earlystart_monthly_seo_sitemap_url()
+{
+	return apply_filters('earlystart_monthly_seo_sitemap_url', home_url('/sitemap.xml'));
+}
+
+/**
+ * Fire-and-forget sitemap ping with strict transport settings.
+ */
+function earlystart_monthly_seo_ping($endpoint, $sitemap_url)
+{
+	$url = add_query_arg('sitemap', $sitemap_url, $endpoint);
+	$url = esc_url_raw($url, array('https'));
+
+	if ($url === '' || (function_exists('wp_http_validate_url') && !wp_http_validate_url($url))) {
+		return false;
+	}
+
+	return wp_remote_get($url, array(
+		'timeout' => 5,
+		'blocking' => false,
+		'sslverify' => true,
+		'reject_unsafe_urls' => true,
+	));
+}
+
+/**
  * Monthly SEO Callback
  * Pings Google and Bing with sitemap URL
  */
@@ -57,21 +96,13 @@ function earlystart_monthly_seo_callback()
 		error_log('[Early Start SEO Cron] Monthly SEO event executed at ' . current_time('mysql'));
 	}
 
-	$sitemap_url = home_url('/?sitemap=xml');
+	$sitemap_url = esc_url_raw(earlystart_monthly_seo_sitemap_url(), array('http', 'https'));
+	if ($sitemap_url === '') {
+		return;
+	}
 
-	// Ping Google
-	wp_remote_get('https://www.google.com/ping?sitemap=' . urlencode($sitemap_url), array(
-		'timeout' => 5,
-		'blocking' => false,
-		'sslverify' => false,
-	));
-
-	// Ping Bing
-	wp_remote_get('https://www.bing.com/ping?sitemap=' . urlencode($sitemap_url), array(
-		'timeout' => 5,
-		'blocking' => false,
-		'sslverify' => false,
-	));
+	earlystart_monthly_seo_ping('https://www.google.com/ping', $sitemap_url);
+	earlystart_monthly_seo_ping('https://www.bing.com/ping', $sitemap_url);
 }
 add_action('earlystart_monthly_seo_event', 'earlystart_monthly_seo_callback');
 
